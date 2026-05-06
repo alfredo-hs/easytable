1c1e3ed158e530a15882bb15f3fa3899d3d47b9c
Alfredo H. Sanchez <alhdzsz@gmail.com>
2026-05-06 23:39:03 +0300

    pipe_value_cran

--- R/pipe.R
+++ R/pipe.R
@@ -1,10 +1,12 @@
 #' Pipe Operator
 #'
-#' Re-export of `%>%` from magrittr.
+#' Re-export of `%>%` from 'magrittr'.
 #'
 #' @name %>%
 #' @rdname pipe
 #' @keywords internal
+#' @return The result of evaluating the right-hand side expression after the
+#'   left-hand side has been passed through the pipe.
 #' @importFrom magrittr %>%
 #' @export
 magrittr::`%>%`
